3ec158864f00ec8ba158b92015b9c5c53bfb0348
Romket <lukehouston08@gmail.com>
2025-01-01 03:17:13 -0700

    Added c++ definitions header

---
+++ include/flashbackclient/defs.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#DEFINE CONFIG_DIR "~/.config/flashbackclient"
+#DEFINE CONFIG_FILE "flashbackclient.yaml"
+#DEFINE CONFIG_FILE_PATH CONFIG_DIR "/" CONFIG_FILE
+
+#DEFINE SCHEDULER_CONFIG_DIR "~/.config/flashbackclient/scheduler"
+#DEFINE SCHEDULER_CONFIG_FILE "scheduler.yaml"
+#DEFINE SCHEDULER_CONFIG_FILE_PATH SCHEDULER_CONFIG_DIR "/" SCHEDULER_CONFIG_FILE
+
+#DEFINE TARGET_DEF_DIR "~/.config/flashbackclient/targets"
+
+#DEFINE LOG_DIR "~/.local/share/flashbackclient"
+#DEFINE LOG_FILE_FMT "%Y-%m-%d_%H-%M-%S"
