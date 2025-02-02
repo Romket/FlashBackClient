5abc38d53b2a32fd1827232c3dba06ec044068c9
Chase Attebury <chaseappleberryboi@gmail.com>
2025-02-02 15:27:26 -0700

    Updated logger.h to include output function, only header though

--- include/flashbackclient/logger.h
+++ include/flashbackclient/logger.h
@@ -13,6 +13,12 @@ namespace FlashBackClient
     public:
         static void Initialize();
         static void SetVerbose();
+        static void const inline OutputTrace();
+        static void const inline OutputDebug();
+        static void const inline OutputInfo();
+        static void const inline OutputWarn();
+        static void const inline OutputError();
+        static void const inline OutputCritical();
 
     };
 } // namespace FlashBackClient
