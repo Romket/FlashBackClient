139c400065a62a39f13a6f5445de085fcc96af4d
Chase Attebury <chaseappleberryboi@gmail.com>
2025-02-04 12:22:52 -0700

    dunno how to make file_sink have backtrace, stashing to store changes in case I bork when merging with main

--- include/flashbackclient/logger.h
+++ include/flashbackclient/logger.h
@@ -10,12 +10,5 @@ namespace FlashBackClient
     public:
         static void Initialize();
         static void SetVerbose();
-
-        static void OutputTrace(const std::string& message);
-        static void OutputDebug(const std::string& message);
-        static void OutputInfo(const std::string& message);
-        static void OutputWarn(const std::string& message);
-        static void OutputError(const std::string& message);
-        static void OutputCritical(const std::string& message);
     };
 } // namespace FlashBackClient
--- src/logger.cpp
+++ src/logger.cpp
@@ -2,6 +2,7 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/async.h>
 
 namespace FlashBackClient
@@ -16,7 +17,7 @@ namespace FlashBackClient
 
         auto console_logger = std::make_shared<spdlog::async_logger>(
             "console", console_sink, spdlog::thread_pool(), spdlog::async_overflow_policy::block);
-        
+
         spdlog::register_logger(console_logger);
 
         console_logger->set_level(spdlog::level::err);
@@ -26,35 +27,5 @@ namespace FlashBackClient
     void Logger::SetVerbose()
     {
         spdlog::get("console")->set_level(spdlog::level::trace);
-    }
-
-    void Logger::OutputTrace(const std::string& message)
-    {
-        spdlog::get("console")->trace(message);
-    }
-
-    void Logger::OutputDebug(const std::string& message)
-    {
-        spdlog::get("console")->debug(message);
-    }
-
-    void Logger::OutputInfo(const std::string& message)
-    {
-        spdlog::get("console")->info(message);
-    }
-
-    void Logger::OutputWarn(const std::string& message)
-    {
-        spdlog::get("console")->warn(message);
-    }
-
-    void Logger::OutputError(const std::string& message)
-    {
-        spdlog::get("console")->error(message);
-    }
-
-    void Logger::OutputCritical(const std::string& message)
-    {
-        spdlog::get("console")->critical(message);
     }
 } // namespace FlashBackClient
