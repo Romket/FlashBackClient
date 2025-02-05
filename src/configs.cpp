5e9c9587f451550f3a21f3276684b819b4402005
Romket <lukehouston08@gmail.com>
2025-02-05 10:29:28 -0700

    remove unncessary include

--- src/configs.cpp
+++ src/configs.cpp
@@ -1,16 +1,11 @@
 #include <flashbackclient/configs.h>
 
-#include <filesystem>
-
 namespace FlashBackClient
 {
-    bool ConfigManager::Initialize()
-    {
-        return SettingManager::Initialize();
-    }
+    bool ConfigManager::Initialize() { return SettingManager::Initialize(); }
 
     void ConfigManager::GenerateConfigs()
     {
         // TODO
     }
-} //namespace FlashBackClient
+} // namespace FlashBackClient
