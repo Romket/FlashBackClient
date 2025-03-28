825650d4c30667a940f7bc6a9e9db4df54ccdd2d
Romket <lukehouston08@gmail.com>
2025-03-28 13:58:41 -0600

    Add license comment to top of files

--- include/flashbackclient/listener.h
+++ include/flashbackclient/listener.h
@@ -1,3 +1,30 @@
+/**
+ * @file listener.h
+ * @author Luke Houston (Romket or RomketBoi) (lukehouston08@gmail.com)
+ * @brief Defines a virtual listener class for file change events
+ * @version 0.1
+ * @date 2025-03-25
+ *
+ * @see platform_listener.h
+ * @sa inotify_listener.h
+ * @sa inotify_listener.cpp
+ *
+ * @copyright Copyright (c) 2025 Luke Houston
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <flashbackclient/condition.h>
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -1,3 +1,34 @@
+/**
+ * @file scheduler.cpp
+ * @author Luke Houston (Romket or RomketBoi) (lukehouston08@gmail.com)
+ * @brief Implements the Scheduler class for scheduling cron expressions and
+ * managing targets and default rules
+ *
+ * @version 0.1
+ * @date 2025-03-28
+ *
+ * @see scheduler.h
+ * @see target.cpp
+ * @sa rulemanager.h
+ * @sa condition.h
+ * @sa rule.h
+ *
+ * @copyright Copyright (c) 2025 Luke Houston
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <flashbackclient/scheduler.h>
 
 #include <flashbackclient/condition.h>
