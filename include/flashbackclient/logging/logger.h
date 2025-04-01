944669b4ed805f278b9eff474486da57466156d1
Chase Attebury <chaseappleberryboi@gmail.com>
2025-03-31 19:38:52 -0600

    * Moved template functions from public to private in logger.h to reflect case and usage

--- include/flashbackclient/logging/logger.h
+++ include/flashbackclient/logging/logger.h
@@ -47,7 +47,15 @@ namespace FlashBackClient
         static void DumpFileLog();
         static void Shutdown();
 
-        // Don't question the std::forward<Args>(args)...
+    private:
+        static std::shared_ptr<DualLevelSink>                     _consoleSink;
+        static std::shared_ptr<spdlog::sinks::CrashFileSink<std::mutex>> _fileSink;
+        static std::shared_ptr<spdlog::logger> _consoleLogger;
+        static std::shared_ptr<spdlog::logger> _fileLogger;
+        // cppcheck-suppress unusedStructMember
+        static std::string _crashFilePath;
+
+                // Don't question the std::forward<Args>(args)...
         // Cause I don't know either
         template<typename... Args>
         static void logTrace(const char* file, int line, const char* func,
@@ -121,14 +129,6 @@ namespace FlashBackClient
                 _fileLogger, "{}:{} [{}] - {}", file, line, func,
                 fmt::format(fmtStr, std::forward<Args>(args)...));
         }
-
-    private:
-        static std::shared_ptr<DualLevelSink>                     _consoleSink;
-        static std::shared_ptr<spdlog::sinks::CrashFileSink<std::mutex>> _fileSink;
-        static std::shared_ptr<spdlog::logger> _consoleLogger;
-        static std::shared_ptr<spdlog::logger> _fileLogger;
-        // cppcheck-suppress unusedStructMember
-        static std::string _crashFilePath;
     };
 } // namespace FlashBackClient
 
